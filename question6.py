af508148af74de45ebc004fed414b210b885cb34
georgettemillimo <georgettemillimo@gmail.com~>
2024-10-23 22:53:32 +0300

    Updated project with the python script on password validation

---
+++ question6.py
@@ -0,0 +1,8 @@
+# user input for a password
+password = input("Enter your password: ")
+
+# Check if the password is python123
+if password == "python123":
+    print("Access granted.")
+else:
+    print("Access denied.")
