7393cbecbb56ab33af84b15c1d240c0ad25eff3c
georgettemillimo <georgettemillimo@gmail.com~>
2024-10-23 21:00:03 +0300

    Updated project with the python script that checks for even or odd numbers

---
+++ question2.py
@@ -0,0 +1,8 @@
+# Ask for user input
+no = int(input("Enter a number: "))
+
+# if statement to Check if the number is even or odd
+if no % 2 == 0:
+    print("The Number "f"{no} is even.")
+else:
+    print("The Number "f"{no} is odd.")
