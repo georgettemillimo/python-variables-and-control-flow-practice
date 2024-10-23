1d03e304f122a05bb17460aa9ba553386c74855d
georgettemillimo <georgettemillimo@gmail.com~>
2024-10-23 20:31:52 +0300

    Python script that performs arithmetic operation using variables

---
+++ question1.py
@@ -0,0 +1,9 @@
+# creation of variables a and b and allocation of values
+a = 10
+b = 5
+
+# Perform arithmetics operations and print results
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
